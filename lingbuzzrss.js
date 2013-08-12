ac31ff18ae11e23d6a63622b1b5dc3860939ae2e
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2013-08-12 01:06:50 -0400

    rm unnecessary modularization of main function; make jshint happy

--- lingbuzzrss.js
+++ lingbuzzrss.js
@@ -1,16 +1,28 @@
 var request = require("request"),
 	cheerio = require("cheerio"),
 	fs = require("fs"),
-	rss = require("rss"),
+	RSS = require("rss"),
 	async = require("async"),
 	url = require("url");
 
 var lingbuzz = 'http://ling.auf.net/lingbuzz',
-	domain = 'http://ling.auf.net';
-
-feed( function(status, body) {
-	console.log(body);
-} );
+	domain = 'http://ling.auf.net',
+	feed = new RSS({
+		title: 'LingBuzz',
+		description: 'archive of linguistics articles',
+		feed_url: 'http://feeds.feedburner.com/LingBuzz',
+		site_url: 'http://ling.auf.net/lingbuzz',
+		// image_url: 'http://example.com/icon.png',
+		// docs: 'http://example.com/rss/docs.html',
+		author: 'LingBuzz',
+		// managingEditor: 'Dylan Greene',
+		webMaster: 'Michael Yoshitaka Erlewine <mitcho@mitcho.com>',
+		// copyright: '2013 Dylan Greene',
+		language: 'en',
+		categories: ['linguistics'],
+		pubDate: 'May 20, 2012 04:00:00 GMT', // todo: fix
+		ttl: '60' // todo: fix?
+	});
 
 function Cache(dir) {
 	this.dir = dir;
@@ -38,7 +50,7 @@ Cache.prototype = {
 				data = JSON.parse(json);
 				console.error('CACHE GET: ' + key);
 				cb(null, data);
-			} catch(err) {
+			} catch (err) {
 				console.error('JSON ERROR: ' + key);
 				cb(err);
 			}
@@ -52,18 +64,18 @@ Cache.prototype = {
 var cache = new Cache('cache');
 
 // function for use with async
-function getFeedItem(entry, cb) {
+function getFeedItem(entryHtml, cb) {
 	var err = null;
 
-	var $ = cheerio.load(entry);
-	var entry = $(entry);
-	var textpart = function(){ return $(this).text().trim(); }
+	var $ = cheerio.load(entryHtml);
+	var entry = $(entryHtml);
+	function textpart() { return $(this).text().trim(); }
 	var authors = entry.find('td:nth-child(1) > a').map(textpart);
 	var status = entry.find('td:nth-child(2)').text().trim();
 	var link = entry.find('td:nth-child(4) > a');
 	var cacheKey = link.attr('href').replace(/^\/lingbuzz\/(\d+)\/?$/, '$1');
 	var href = url.resolve(domain, link.attr('href'));
-	var source = url.parse(href, true).query['repo'] || 'lingbuzz';
+	var source = url.parse(href, true).query.repo || 'lingbuzz';
 	
 	var freshFeedItemStub = {
 		title: link.text(),
@@ -102,7 +114,7 @@ function getFeedItem(entry, cb) {
 
 		cache.set(cacheKey, freshFeedItemStub, function(err) {
 			cb(err, freshFeedItemStub);
-		})
+		});
 	}
 	
 	if (source == 'lingbuzz') {
@@ -120,7 +132,6 @@ function getFeedItem(entry, cb) {
 			} else if ( feedItem.title !== freshFeedItemStub.title ||
 				feedItem.author !== freshFeedItemStub.author ) {
 				console.error('BASIC DATA MISMATCH: ' + cacheKey);
-				console.error(feedItem.title,freshFeedItemStub.title,feedItem.author,freshFeedItemStub.author)
 				console.error('GET ' + href + ' ...');
 				request(href, parseEntry);			
 			} else {
@@ -132,50 +143,29 @@ function getFeedItem(entry, cb) {
 	}
 }
 
-function feed( cb ) {
-	var feed = new rss({
-		title: 'LingBuzz',
-		description: 'archive of linguistics articles',
-		feed_url: 'http://mitcho.com/rss/lingbuzz.xml',
-		site_url: 'http://ling.auf.net/lingbuzz',
-		// image_url: 'http://example.com/icon.png',
-		// docs: 'http://example.com/rss/docs.html',
-		author: 'LingBuzz',
-		// managingEditor: 'Dylan Greene',
-		webMaster: 'Michael Yoshitaka Erlewine <mitcho@mitcho.com>',
-		// copyright: '2013 Dylan Greene',
-		language: 'en',
-		categories: ['linguistics'],
-		pubDate: 'May 20, 2012 04:00:00 GMT', // todo: fix
-		ttl: '60' // todo: fix?
-	});
-
-	console.error('GET ' + lingbuzz + ' ...');
-	request(lingbuzz, buzzed);
-	function buzzed(err, res, body) {
-		if (err) {
-			cb(503, '');
-			console.error(err);
-		}
-	
-		if (res.statusCode != 200) {
-			// proxy the same status code:
-			console.error(res.statusCode, http.STATUS_CODES[res.statusCode]);
-			cb(res.statusCode,'');
-		}
-	
-		// load cheerio, the faux-jQuery, for the body html
-		var $ = cheerio.load(body);
-		var entries = $('table table').first().find('tr');
-
-		async.map(entries.toArray(), getFeedItem, function(err, results) {
-			results.forEach(function(feedItem) {
-				console.error(feedItem);
-				if (feedItem.source == 'lingbuzz')
-					feed.item(feedItem);
-			});
-
-			cb(200, feed.xml());
-		})
+console.error('GET ' + lingbuzz + ' ...');
+request(lingbuzz, function(err, res, body) {
+	if (err) {
+		console.error(err);
+		return;
 	}
-}
+
+	if (res.statusCode != 200) {
+		console.error(res.statusCode, http.STATUS_CODES[res.statusCode]);
+		return;
+	}
+
+	// load cheerio, the faux-jQuery, for the body html
+	var $ = cheerio.load(body);
+	var entries = $('table table').first().find('tr');
+
+	async.map(entries.toArray(), getFeedItem, function(err, results) {
+		results.forEach(function(feedItem) {
+			console.error(feedItem);
+			if (feedItem.source == 'lingbuzz')
+				feed.item(feedItem);
+		});
+
+		console.log(200, feed.xml());
+	});
+});
