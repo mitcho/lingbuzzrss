04de30f4bfe9eaf49367fe50801e6078838137c6
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2015-09-24 15:35:12 +0200

    rm bad characters from hashtags

--- lingbuzzrss.js
+++ lingbuzzrss.js
@@ -223,7 +223,7 @@ request({url: LINGBUZZ, headers: HEADERS}, function(err, res, body) {
 				
 				// add hashtags:
 				var twitterCategories = feedItem.categories.map(function (x) {
-					return x.replace(/\s/,'');
+					return x.replace(/[^\w_]/,'');
 				});
 				while (twitterCategories.length) {
 					var consider = twitterCategories.shift();
